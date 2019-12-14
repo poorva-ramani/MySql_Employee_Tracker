5bfb26aa2018914d8053c6c7bd5bc5b44a1762f0
Poorva Ramani <poorva.ramani@washpost.com>
2019-12-14 14:41:14 -0600

    updating departments table and implementing console.table

---
+++ config/db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO department SET name='Sales';
+INSERT INTO department SET name='Engineering';
+INSERT INTO department SET name='Finance';
+INSERT INTO department SET name='Legal';
+INSERT INTO department SET name='HR';
